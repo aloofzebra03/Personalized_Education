3a1d31308554283e0591ce5b6b2a3ada27c93654
Aryan Tushar Singhal <aryansinghalaattss@gmail.com>
2025-05-24 21:06:21 +0530

    api gone

--- Judging_Personalization/config.py
+++ Judging_Personalization/config.py
@@ -3,7 +3,7 @@ from google import genai# — Google API client —
 from dotenv import load_dotenv
 load_dotenv()
 GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
-print(GOOGLE_API_KEY)
+# print(GOOGLE_API_KEY)
 client = genai.Client(api_key=GOOGLE_API_KEY)
 # The Gemini model we'll use for scoring
 MODEL_NAME = "gemini-2.0-flash"
