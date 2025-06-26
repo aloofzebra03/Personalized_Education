3d26a73eaa52e50006971dd176ad5f21b73f9e5e
Aryan Tushar Singhal <aryansinghalaattss@gmail.com>
2025-06-26 21:33:26 +0530

    Update config.py

--- Creating_Section_Params/config.py
+++ Creating_Section_Params/config.py
@@ -5,7 +5,10 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MODEL_NAME     = "gemini-2.0-flash"
-GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+import streamlit as st
+
+GOOGLE_API_KEY = st.secrets.get("GOOGLE_API_KEY")
+# GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 STUDENT_PARAMS_CSV = "Creating_Section_Params/data/langchain_student_params_1500_reduced.csv"
 SECTION_PARAMS_CSV = "Creating_Section_Params/data/next_section_ranges_chatgpt.csv.csv"
