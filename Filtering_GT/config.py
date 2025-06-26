5f5b73956c33eef32ac088487dca49e46eaee693
Aryan Tushar Singhal <aryansinghalaattss@gmail.com>
2025-06-26 22:04:05 +0530

    Update config.py

--- Filtering_GT/config.py
+++ Filtering_GT/config.py
@@ -6,7 +6,7 @@ load_dotenv()
 import streamlit as st
 
 GOOGLE_API_KEY = st.secrets.get("GOOGLE_API_KEY")
-MODEL_NAME     = "gemini-2.0-flash"
+MODEL_NAME     = "gemini-1.5-flash"
 # GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 STUDENT_PARAMS_CSV = "Creating_Section_Params/data/langchain_student_params_1500_reduced.csv"
