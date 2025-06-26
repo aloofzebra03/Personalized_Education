543092ed28fbce12c3a5a9dc4a00d67aca201b11
Aryan Tushar Singhal <aryansinghalaattss@gmail.com>
2025-06-26 15:20:31 +0530

    Update embedder.py

--- Creating_Section_Text/embedder.py
+++ Creating_Section_Text/embedder.py
@@ -5,15 +5,14 @@ from langchain_google_genai import GoogleGenerativeAIEmbeddings
 # make absolutely sure your key is set before any gRPC code loads:
 os.environ["GOOGLE_API_KEY"]     = config.GOOGLE_API_KEY
 
-os.environ["GOOGLE_API_USE_REST"] = "true"
-os.environ["GRPC_DNS_RESOLVER"] = "native"
+# os.environ["GOOGLE_API_USE_REST"] = "true"
+# os.environ["GRPC_DNS_RESOLVER"] = "native"
 
 
 def get_embedder():
     print("Building Gemini embedder...")
     embeddings = GoogleGenerativeAIEmbeddings(
         model="models/embedding-001",  # <- NO "models/" prefix
-        transport="http",                    # <- HTTP/1.1 path only
         google_api_key=os['GOOGLE_API_KEY'] # <- explicit auth
     )
     # sanity check
