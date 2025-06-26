51f1c995fd1f9ddab1e80690c45f1a4950824328
Aryan Tushar Singhal <aryansinghalaattss@gmail.com>
2025-06-26 13:25:03 +0530

    updates

--- packages.py
+++ packages.py
@@ -13,30 +13,8 @@ for pkg in data:
     n, v = pkg["name"], pkg["version"]
     if n.lower() in ("pip", "setuptools", "wheel"):
         continue
-    reqs.append(f"{n}=={v}")
-
-with open("requirements.txt", "w", encoding="utf-8") as out:
-    out.write("\n".join(sorted(reqs)))
-
-print("✔ wrote requirements.txt")
-
-
-# import json, sys
-
-fname = "pkgs.json"
-try:
-    # 'utf-8-sig' will silently drop a BOM if present
-    with open(fname, encoding="utf-8-sig") as f:
-        data = json.load(f)
-except json.JSONDecodeError as e:
-    sys.exit(f"ERROR: couldn’t parse {fname} as JSON: {e}")
-
-reqs = []
-for pkg in data:
-    n, v = pkg["name"], pkg["version"]
-    if n.lower() in ("pip", "setuptools", "wheel"):
-        continue
-    reqs.append(f"{n}=={v}")
+    # reqs.append(f"{n}=={v}")
+    reqs.append(f"{n}>= {v}")  # use >= to allow minor updates
 
 with open("requirements.txt", "w", encoding="utf-8") as out:
     out.write("\n".join(sorted(reqs)))
